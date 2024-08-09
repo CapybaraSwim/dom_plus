48822d175f66515cd1a7b54fc0687678dc106838
VladimirPetrosyan <petrosyan412@icloud.com>
2024-08-09 20:02:24 +0300

    изменил ссылку на токен

--- src/components/Cookies.js
+++ src/components/Cookies.js
@@ -10,7 +10,7 @@ const AmoCRMIntegration = () => {
 
     const getAccessToken = async () => {
         try {
-            const response = await fetch('https://petrosyan412.amocrm.ru/oauth2/access_token', {
+            const response = await fetch('https://petrosyan412.amocrm.ru/leads/pipeline/?skip_filter=Y', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -19,7 +19,7 @@ const AmoCRMIntegration = () => {
                     client_id: '8e041ea2-53d3-45f8-ba74-1c43e8e1e648', // Ваш ID интеграции
                     client_secret: 'vH5UvdYAgJ9ULE4qr7ynAh1HSEKeyXcEiFAlgUzK5a54Wo2PR6dXgD3Ghei50SM5', // Ваш секретный ключ
                     grant_type: 'authorization_code',
-                    code: 'def5020051fe4dcb64d369b65d8df45a7e220c813e22159a8c7669b6cace85a45a7f49032a46a08471844d16336a380b8147583a7ea82cd679308a6e9dcce673d66011ddc5b383fb342245d4c3de1e15788aad878b855cc1d01488a32fe26279eda2f7396a61df20d17b19ef4c6af06b72771f115f69b8a5ab98e35a2fd30a5ee7790be0f88864e5f2fdc1fc690d718829e61b680decbcf5b506ce7f7992848cc1841b1f6278f14706c6bd1e03db5ed52f4e67196ad2cd40a59f0a7e07de3cb56cc8adeb4722d9217a9e29c449c861dde9b56324c1356887fd285756ba97f633b7922f982e2218174b6c370f51147e0988c4f5189f2722291739a8616326d06656acef981158c5795f201878c0b002af2f4c801831662ae352c460e49cb3a882d220d6588fc4bbfd45fbb6469e91ad59b04dbeac445ea0cacfc353c18a0057aa3a7935af392a6fc309caca551fae9d260f7316260a6682a5482efffe17d923078941722fe47ecd90aa0841d36443028e87b0cfd6f2fc0e028d4d3f242439e5a09405004637c902a9e70c0403d5a0da8a81ae295ff39059a02346576ebfe23c9674cd91863b67f4b10bca231ef0795023c67024115f3d5c04f053c05e5eb73e2a77feaf29193ddf89e5ac4d0443ffb12804012cabb44a51f754a024c2a85710e5c194c2d328efc8788fc3bdeb5b', // Ваш код авторизации
+                    code: 'def5020078f7954569089ec7d70933524214fe8ce406114cd255b2da64e9ceccd8fb63e062a59027f666f9654202b527909a35e0546b3e57c4353c95103f83430feb2d11e58b2a2ec416ec1b9e757614f3413154915af9e65937b426de27ac60952b0492bc36ef0be6190969571c144a48d7daa013fb516069dcfa4cfbfc0ae12e6f676d281aae23de7da0ed727ff04bbfc504f0bacef431fd2a84c7b0d1722be7f623db9aa527bdeaded642f1d02ceb59ad6aaf9a21d5d39cd6d99b879b7460acc85098effd422923e16d6670d4c1a904dbe59c8528d840ea449fa4e6bfe5c999d2c9d0f19b18624d82decfc32068ccc963a2cc84574150a15bae5ee03f5c439a441071b925bb8ff71bb36fe524d877ff800dbb6ab183eff0a861cbcf6e7c7931ca25ba6daf50e99e380f959b36b2a5aec3d75588ffde722c24ef5c98f0640abf44be201473b7ab5d5004b9beda9794d83bce9b2c206b39d061db457d82465fa2d763b0dbd1ae691d65b8b0a7016fd70a921b969fb6aacdebefb7d523e9a88cec61e4ad60ca5c5dc41331a8a1db8513d45a4882a5c50c3209bcd13b9aa94fb5f160993ff272875dd3ae8db024d5a15d4a25e63c0e3ede95a179ce564f2dae792df25e3c304581bb2af2295d5c690a89ae9a4c68a0c5d6c0013c640bc111719abaf3e6c829cb1ac980bd891fca', // Ваш код авторизации
                     redirect_uri: 'https://домплюс.com/', // Ваш Redirect URI (должен совпадать с настройками интеграции)
                 }),
             });
@@ -41,7 +41,7 @@ const AmoCRMIntegration = () => {
 
     const getCustomFields = async (token) => {
         try {
-            const response = await fetch('https://petrosyan412.amocrm.ru/api/v4/leads/custom_fields', {
+            const response = await fetch('https://petrosyan412.amocrm.ru/leads/pipeline/?skip_filter=Y', {
                 method: 'GET',
                 headers: {
                     'Authorization': `Bearer ${token}`,
