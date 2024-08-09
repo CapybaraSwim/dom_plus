022e98e376d511a0f7c0101a82481dd182a9a870
VladimirPetrosyan <petrosyan412@icloud.com>
2024-08-09 16:32:04 +0300

    import useEffect
    импортирую юз эффекты

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './index.scss';
 import HomePage from './components/HomePage';
 import PresentationPage from './components/PresentationPage';
