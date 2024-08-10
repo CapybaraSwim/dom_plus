60c7d918782e75e348b3b907205bd78989d54aca
VladimirPetrosyan <petrosyan412@icloud.com>
2024-08-10 13:01:30 +0300

    добавил дебаг

--- src/components/Cookies.js
+++ src/components/Cookies.js
@@ -10,17 +10,19 @@ const createLead = async () => {
         // другие данные сделки
     };
 
-    const response = await fetch('https://petrosyan412.amocrm.ru/api/v4/leads', {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify([leadData]),
-    });
-
-    if (response.ok) {
+    try {
+        const response = await fetch('https://petrosyan412.amocrm.ru/api/v4/leads', {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify([leadData]),
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
         const data = await response.json();
         console.log('Заявка успешно отправлена:', data);
-    } else {
-        console.error('Ошибка при отправке заявки:', response.statusText);
+    } catch (error) {
+        console.error('Ошибка при отправке заявки:', error.message);
     }
 };
 
